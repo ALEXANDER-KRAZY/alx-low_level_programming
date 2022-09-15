bfac06afb010be337f0a3734c4eb83065f947f3e
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2022-09-15 11:06:32 -0700

    another commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	for (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
