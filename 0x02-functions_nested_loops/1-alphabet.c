f1e0ed78906b3caff049993084cd27bc371d5870
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2022-09-15 06:52:00 -0700

    another commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  * print_alphabet - print the alphabet in lowercase
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char a;
 	for(a='a'; a<='z'; a++)
 	{
-		_putchar(a);
+		_putchar("%c", a);
 	}
 		_putchar("\n");
 }
