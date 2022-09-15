9c4c4e99418d6c67cd017f4ffedc5d160f593045
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2022-09-15 06:23:12 -0700

    another commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#includ "main.h"
+#include "main.h"
 /**
- * main - print putchar
- * Return: 0
+ *main- Print string __putchar as the output
+ *Return: 0
  */
 int main(void)
 {
-	_putchar("_putchar");
-	return(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
