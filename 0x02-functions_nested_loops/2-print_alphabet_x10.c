729eb2deecfffd481dcd0d8f9a153dcb78dc43dd
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2022-09-15 07:08:43 -0700

    another commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prins 10 times the alphabet, in lowercase
+ * Retun: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
